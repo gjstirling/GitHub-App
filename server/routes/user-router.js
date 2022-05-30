c53f08685c24698af25adf1f55c72742f25418e0
Graeme Stirling <graemejstirling@gmail.com>
2022-05-30 16:08:31 +0100

    Adding login authetication and JWT token

--- server/routes/user-router.js
+++ server/routes/user-router.js
@@ -1,42 +1,67 @@
 const express = require("express");
 const router = express.Router();
+const validateUser = require("../middleware/user-middleware.js");
+const User = require("../models/user-model");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 router.post("/user", validateUser, (req, res) => {
-    res.status(200).send("user added");
+  const newUser = new User({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    password: req.body.password,
+  });
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(newUser.password, salt, (err, hash) => {
+      if (err) throw err;
+      newUser.password = hash;
+      newUser
+        .save()
+        .then((user) => res.json(user))
+        .catch((err) => console.log(err));
+    });
+  });
 });
 
-function validateUser(req, res, next) {
-    const user = req.body;
-    if (user.firstName.trim() === "") {
-        return res.status(400).send("First name is a required field");
+router.get("/user", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  User.findOne({ email }).then((user) => {
+    // Check if user exists
+    if (!user) {
+      return res.status(404).json({ emailnotfound: "Email not found" });
     }
-    if (user.lastName.trim() === "") {
-        return res.status(400).send("Last name is a required field");
-    }
-    if (!validateEmail(user.email)){
-        return res.status(400).send("Please use a valid email address");
-    }
-    if (!validatePassword(user.password)){
-        return res.status(400).send("Password format wrong");
-    }
-
-    next();
-}
-
-function validateEmail(email){
-    return String(email)
-      .toLowerCase()
-      .match(
-        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-      );
-  };
-
-function validatePassword(password){
-    return String(password )
-      .toLowerCase()
-      .match(
-        /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/
-      );
-}
+    // Check password
+    bcrypt.compare(password, user.password).then((isMatch) => {
+      if (isMatch) {
+        // User matched
+        // Create JWT Payload
+        const payload = {
+          id: user.id,
+          firstname: user.firstname,
+        };
+        // Sign token
+        jwt.sign(
+          payload,
+          "secret",
+          {
+            expiresIn: 31556926, // 1 year in seconds
+          },
+          (err, token) => {
+            res.json({
+              success: true,
+              token: "Bearer " + token,
+            });
+          }
+        );
+      } else {
+        return res
+          .status(400)
+          .json({ passwordincorrect: "Password incorrect" });
+      }
+    });
+  });
+});
 
 module.exports = router;

a77579a061cf9832101b864ca408190c02b82a80
Graeme Stirling <graemejstirling@gmail.com>
2022-06-13 10:29:28 +0100

    Cleaning up router (extracting middleware logic out)

--- server/middleware/user-middleware.js
+++ server/middleware/user-middleware.js
@@ -1,4 +1,8 @@
-module.exports = function validateUser(req, res, next) {
+const bcrypt = require("bcryptjs")
+const User = require("../models/user-model");
+const jwt = require("jsonwebtoken");
+
+validateUser = (req, res, next) => {
     const user = req.body;
     if (user.firstName.trim() === "") {
         return res.status(400).send("First name is a required field");
@@ -15,6 +19,64 @@ module.exports = function validateUser(req, res, next) {
     next();
 }
 
+createUser = (req, res) => {
+  const newUser = new User({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    password: req.body.password,
+  });
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(newUser.password, salt, (err, hash) => {
+      if (err) throw err;
+      newUser.password = hash;
+      newUser
+        .save()
+        .then((user) => res.json(user))
+        .catch((err) => console.log(err));
+    });
+  });
+}
+
+verifyUser = (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  User.findOne({ email }).then((user) => {
+    if (!user) {
+      return res.status(401).json({ error: "Email or password incorrect" });
+    }
+    // Check password
+    bcrypt.compare(password, user.password).then((isMatch) => {
+      if (isMatch) {
+        // User matched
+        // Create JWT Payload
+        const payload = {
+          id: user.id,
+          firstname: user.firstName,
+        };
+        // Sign token
+        jwt.sign(
+          payload,
+          "secret",
+          {
+            expiresIn: 86400, // 1 day in seconds
+          },
+          (err, token) => {
+            res.json({
+              success: true,
+              token: token,
+            });
+          }
+        );
+      } else {
+        return res
+          .status(401)
+          .json({ error: "Email or password incorrect" });
+      }
+    });
+  });
+}
+
 function validateEmail(email){
     return String(email)
       .toLowerCase()
@@ -29,4 +91,10 @@ function validatePassword(password){
       .match(
         /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,30}$/
       );
+}
+
+module.exports = {
+  validateUser,
+  createUser,
+  verifyUser
 }
